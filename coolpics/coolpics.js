5af9702549117f23b4ec4298ffcbe3ffcd32c591
Will7332 <will7332@byui.edu>
2025-02-06 16:21:44 -0700

    Debugged and updated Cool Pics page

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,9 +1,9 @@
 const menuButton = document.querySelector(".menu-button");
 
-
+const menu = document.querySelector(".menu");
 function toggleMenu() {
 
-    const menu = document.querySelector(".menu");
+
     menu.classList.toggle("hide");
 }
 
@@ -32,23 +32,24 @@ window.addEventListener("resize", handleResize);
 function viewerTemplate(pic, alt) {
     return `<div class="viewer">
     <button class="close">X</button>
-    <img src="${pic}" alt="${alt}">
+    <img class="enlarged" src="${pic}" alt="${alt}">
     </div>`;
 }
 
 function viewHandler(event) {
+    console.log("function called")
     // create a variable to hold the element that was clicked on from event.target
-    let clicked = event.target;
+    const clicked = event.target;
 
     // get the src attribute from that element and 'split' it on the "-"
     if (clicked.tagName ==="IMG") {
-        let imgsrc = clicked.src;
+        const imgsrc = clicked.src;
 
-    let Splitsrc = imgsrc.split("-");
+    const Splitsrc = imgsrc.split("-");
 
     // construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
 
-    let newImgsrc = Splitsrc[0] + "-full.jpeg";
+    const newImgsrc = Splitsrc[0] + "-full.jpeg";
     // insert the viewerTemplate into the top of the body element
     // (element.insertAdjacentHTML("afterbegin", htmltoinsert))
 
@@ -58,12 +59,13 @@ function viewHandler(event) {
 
     document.querySelector(".close").addEventListener("click", closeViewer);
     }
+
 }
 
 function closeViewer(){
     document.querySelector(".viewer").remove();
 }
 
-let gallery= document.getElementById("gallery");
+let gallery= document.querySelector(".gallery");
 
 gallery.addEventListener("click", viewHandler);
